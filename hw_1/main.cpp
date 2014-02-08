c723b52bf4a29574c8283f4a48891dc6436b6545
Samir Silbak <silbak04@gmail.com>
2014-02-08 00:33:04 -0500

    using greedy pair - work in progress

--- hw_1/main.cpp
+++ hw_1/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <algorithm>
 #include <unordered_set>
+#include <set>
 #include <vector>
 #include <ctime>
 
@@ -32,9 +33,10 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
         {
             d_value[cell] = 0;
 
-            for (auto it = connections[cell].begin(); it != connections[cell].end(); it++)
+            //for (auto it = connections[cell].begin(); it != connections[cell].end(); it++)
+            for (auto &node : connections[cell])
             {
-                int node = *it;
+                //int node = *it;
 
                 const bool cell_in_a = group_a.find(cell) != group_a.end();
                 const bool cell_in_b = group_b.find(cell) != group_b.end();
@@ -44,7 +46,7 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
                 if (cell_in_a == cell_in_b)
                     printf("[error]: cannot have two of the same cells in different groups\n");
 
-                // is cell in group a and node2 in group b (an external connection)
+                // is cell in group a and node in group b (an external connection)
                 // vice versa? if so, increment by 1, else decrement by 1.
                 if ((cell_in_a && node_in_b) || (cell_in_b && node_in_a))
                     d_value[cell] += 1;
@@ -52,13 +54,6 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
                     d_value[cell] -= 1;
             }
         }
-
-#ifdef DEBUG
-        for (int cell = 1; cell < num_cells+1; cell++)
-        {
-            printf("d[%02d] = [%02d]\n", cell, d_value[cell]);
-        }
-#endif
 
         int *gains;
         int *max_node_a;
@@ -75,70 +70,84 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
         int max_rduct_cost = 0;
         int reduction_cost = 0;
 
+        int d      = INT_MIN;
+        int dmax_a = INT_MIN;
+        int dmax_b = INT_MIN;
+
+        int max_a = -1;
+        int max_b = -1;
+
         // if we find max gain between cells, then
         // let's mark it as locked (do not swap),
         // swap other nodes that are not locked
-        for (int cell = 1; cell < (num_cells/2)+1; cell++)
+        // we will be using the greedy pair method
+        for (int i = 1; i < (num_cells/2)+1; i++)
         {
-            reduction_cost   = 0;
-            max_rduct_cost   = INT_MIN;
-            max_node_a[cell] = 0;
-            max_node_b[cell] = 0;
+            //d              = INT_MIN;
+            dmax_a         = INT_MIN;
+            dmax_b         = INT_MIN;
+            max_rduct_cost = INT_MIN;
+            //reduction_cost = INT_MIN;
 
-            for (auto it = group_a1.begin(); it != group_a1.end(); it++)
+            bool node_in_a1 = false;
+            bool node_in_b1 = false;
+
+            for (auto &node_a1 : group_a1)
             {
-                for (auto it2 = group_b1.begin(); it2 != group_b1.end(); it2++)
+                node_in_a1 = group_a1.find(i) != group_a1.end();
+
+                if (lock[node_a1]) continue;
+
+                d = d_value[node_a1];
+                if (d > dmax_a)
                 {
-                    int node_a1 = *it;
-                    int node_b1 = *it2;
-
-                    const bool cell_in_a1 = group_a1.find(cell   ) != group_a1.end();
-                    const bool cell_in_b1 = group_b1.find(cell   ) != group_b1.end();
-                    const bool node_in_a1 = group_a1.find(node_b1) != group_a1.end();
-                    const bool node_in_b1 = group_b1.find(node_a1) != group_b1.end();
-
-                    if (lock[node_a1] || lock[node_b1]) continue;
-
-                    if (cell_in_a1 == cell_in_b1)
-                        printf("[error]: cannot have two of the same cells in different groups\n");
-
-                    if ((cell_in_a1 && node_in_b1) || (cell_in_b1 && node_in_a1))
-                        reduction_cost = d_value[node_a1] + d_value[node_b1] - 2;
-                    else
-                        reduction_cost = d_value[node_a1] + d_value[node_b1];
-
-                    if (reduction_cost > max_rduct_cost){
-                        max_rduct_cost   = reduction_cost;
-                        max_node_a[cell] = node_a1;
-                        max_node_b[cell] = node_b1;
-                    }
+                    dmax_a = d;
+                    max_a  = node_a1;
                 }
             }
+            for (auto &node_b1 : group_b1)
+            {
+                node_in_b1 = group_b1.find(i) != group_b1.end();
 
-            // store all the gains for each vertex pair
-            gains[cell] = max_rduct_cost;
+                if (lock[node_b1]) continue;
+
+                d = d_value[node_b1];
+                if (d > dmax_b)
+                {
+                    dmax_b = d;
+                    max_b  = node_b1;
+                }
+            }
+            if ((node_in_a1) && (node_in_b1))
+                reduction_cost = dmax_a + dmax_b - 2;
+            else
+                reduction_cost = dmax_a + dmax_b;
+
+            if (reduction_cost > max_rduct_cost){
+                max_rduct_cost = reduction_cost;
+                max_node_a[i]  = max_a;
+                max_node_b[i]  = max_b;
+            }
+            gains[i] = max_rduct_cost;
 
             // swap a and b
-            group_a1.erase( max_node_a[cell]);
-            group_b1.erase( max_node_b[cell]);
-            group_a1.insert(max_node_b[cell]);
-            group_b1.insert(max_node_a[cell]);
+            group_a1.erase( max_node_a[i]);
+            group_b1.erase( max_node_b[i]);
+            group_a1.insert(max_node_b[i]);
+            group_b1.insert(max_node_a[i]);
 
             // lock nodes
-            lock[max_node_a[cell]] = 1;
-            lock[max_node_b[cell]] = 1;
+            lock[max_node_a[i]] = 1;
+            lock[max_node_b[i]] = 1;
 
             // now we need to update
             // dvalues for group a1/b1
-            // inlining for now, but will clean up so i can pass sets into function
             for (int cell = 1; cell < num_cells+1; cell++)
             {
                 d_value[cell] = 0;
 
-                for (auto it = connections[cell].begin(); it != connections[cell].end(); it++)
+                for (auto &node : connections[cell])
                 {
-                    int node = *it;
-
                     const bool cell_in_a1 = group_a1.find(cell) != group_a1.end();
                     const bool cell_in_b1 = group_b1.find(cell) != group_b1.end();
                     const bool node_in_a1 = group_a1.find(node) != group_a1.end();
@@ -147,7 +156,7 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
                     if (cell_in_a1 == cell_in_b1)
                         printf("[error]: cannot have two of the same cells in different groups\n");
 
-                    // is cell in group a and node2 in group b (an external connection)
+                    // is cell in group a and node in group b (an external connection)
                     // vice versa? if so, increment by 1, else decrement by 1.
                     if ((cell_in_a1 && node_in_b1) || (cell_in_b1 && node_in_a1))
                         d_value[cell] += 1;
@@ -158,16 +167,16 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
         }
 
 #ifdef DEBUG
-        for (int cell = 1; cell < num_cells+1; cell++)
+        for (int i = 1; i < num_cells+1; i++)
         {
-            printf("d[%02d] = [%02d]\n", cell, d_value[cell]);
+            printf("d[%02d] = [%02d]\n", i, d_value[i]);
         }
         printf("--------------------------------\n");
+#endif
         for (int i = 1; i < (num_cells/2)+1; i++)
         {
             printf("max reduction cost[%d] (%d, %d) = %d\n", i, max_node_a[i], max_node_b[i], gains[i]);
         }
-#endif
 
         int g_max = INT_MIN;
         int g_sum = 0;
@@ -185,11 +194,11 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
             k_val += 1;
         }
 
-#ifdef DEBUG
+//#ifdef DEBUG
         printf("------------------------------------\n");
         printf("g_sum = %d | g_max = %d | k_max = %d\n", g_sum, g_max, k_max);
         printf("------------------------------------\n");
-#endif
+//#endif
 
         // keep looping until g_max
         // is no longer > 0
@@ -197,19 +206,20 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
 
         // we must exchange a[1]...a[k_max]
         // with b[1]...b[k_max]
-        for (int i = 1; i < k_max+1; i++)
+        for (int k = 1; k < k_max+1; k++)
         {
 
 #ifdef DEBUG
-            printf("exchanging[%d]: (%d, %d)\n", i, max_node_a[i], max_node_b[i]);
+            printf("exchanging[%d]: (%d, %d)\n", k, max_node_a[k], max_node_b[k]);
 #endif
-            group_a.erase( max_node_a[i]);
-            group_b.erase( max_node_b[i]);
-            group_a.insert(max_node_b[i]);
-            group_b.insert(max_node_a[i]);
+            group_a.erase( max_node_a[k]);
+            group_b.erase( max_node_b[k]);
+            group_a.insert(max_node_b[k]);
+            group_b.insert(max_node_a[k]);
         }
 
-        delete [] max_node_a, max_node_b;
+        delete [] max_node_a;
+        delete [] max_node_b;
         delete [] lock;
     }
 
@@ -219,10 +229,8 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
     int cut_size = 0;
     for (int cell = 1; cell < num_cells+1; cell++)
     {
-        for (auto it = connections[cell].begin(); it != connections[cell].end(); it++)
+        for (auto &node : connections[cell])
         {
-            int node = *it;
-
             const bool cell_in_a1 = group_a1.find(cell) != group_a1.end();
             const bool node_in_b1 = group_b1.find(node) != group_b1.end();
 
@@ -241,9 +249,8 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
     // group a
     for (int cell = 0; cell < (num_cells/2); cell++)
     {
-        for (auto it = group_a1.begin(); it != group_a1.end(); it++)
+        for (auto &node_a : group_a1)
         {
-            int node_a = *it;
             a.push_back(node_a);
         }
     }
@@ -252,9 +259,8 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
     sort(a.begin(), a.end());
     a.erase(unique(a.begin(), a.end()), a.end());
 
-    for (auto it = a.begin(); it != a.end(); it++)
+    for (auto &node_a : a)
     {
-        int node_a = *it;
         printf("%d ", node_a);
     }
     printf("\n");
@@ -262,9 +268,8 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
     // group b
     for (int cell = 0; cell < (num_cells/2); cell++)
     {
-        for (auto it = group_b1.begin(); it != group_b1.end(); it++)
+        for (auto &node_b : group_b1)
         {
-            int node_b = *it;
             b.push_back(node_b);
         }
     }
@@ -273,18 +278,33 @@ int compute_cutset(unordered_set<int> *connections, unordered_set<int> group_a, 
     sort(b.begin(), b.end());
     b.erase(unique(b.begin(), b.end()), b.end());
 
-    for (auto it = b.begin(); it != b.end(); it++)
+    for (auto &node_b : b)
     {
-        int node_b = *it;
         printf("%d ", node_b);
     }
     printf("\n");
+    /*
+    printf("a: ");
+    for (auto &node_a : group_a1)
+    {
+        printf("%d ", node_a);
+    }
+    printf("\n");
+    printf("b: ");
+    for (auto &node_b : group_b1)
+    {
+        printf("%d ", node_b);
+    }
+    printf("\n");
+    */
 
     delete [] connections;
     delete [] d_value;
 
     end_time = clock();
     printf("execution time = %0.3fs\n", (end_time - start_time) / CLOCKS_PER_SEC);
+
+    return cut_size;
 }
 
 int main(int argc, char **argv)
@@ -334,6 +354,14 @@ int main(int argc, char **argv)
 #ifdef DEBUG
     printf("number of cells: %d\n", num_cells);
     printf("number of nets:  %d\n", num_nets);
+
+    for (int i = 1; i < num_cells+1; i++)
+    {
+        for (auto &j : connection[i])
+        {
+            printf("node [%02d] -> [%02d]\n", i, j);
+        }
+    }
 #endif
 
     // split cells into two groups - every other one
@@ -346,17 +374,6 @@ int main(int argc, char **argv)
     }
 
     compute_cutset(connection, group_a, group_b, num_cells);
-
-#ifdef DEBUG
-    for (int i = 1; i < num_cells+1; i++)
-    {
-        for (auto it = connection[i].begin(); it != connection[i].end(); it++)
-        {
-            int j = *it;
-            printf("node [%02d] -> [%02d]\n", i, j);
-        }
-    }
-#endif
 
     return 0;
 }
